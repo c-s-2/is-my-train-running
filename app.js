94f91595b419b6372719028fbfb4d654f9087c8b
Chris Simpson <chris.simpson@onthebeach.co.uk>
2020-01-19 21:08:47 +0000

    Add .env for environment variables

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const http = require('http');
+const dotenv = require('dotenv').config();
 const fetch = require('node-fetch');
 const fs = require('fs');
+const http = require('http');
+
+const Error = require('./error.js');
 const Template = require('./template.js');
 
+const amTrainId = "C72916";
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const amTrainId = "C72916";
-
-const getTrains = () => {
-  return fetch('http://transportapi.com/v3/uk/train/station/tiploc:STKP/live.json?app_id=[id]&app_key=[key]')
+const getDepartures = () => {
+  return fetch(`http://transportapi.com/v3/uk/train/station/tiploc:STKP/live.json?app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`)
   .then(response => response.json())
   .then(response => response);
 };
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const path = req.url;
 
   if (path === '/style.css') {
@@ -23,30 +25,37 @@ const server = http.createServer((req, res) => {
     res.write(fs.readFileSync('./style.css', { encoding: 'utf8' }));
     res.end();
   } else {
-    const data = await getTrains();
-    const morningTrain = data.departures.all.find(train => train.train_uid === morningId);
-    const {
-      aimed_departure_time: aimedDepartureTime,
-      destination_name: destination,
-      expected_departure_time: expectedDepartureTime,
-      operator_name: operator,
-      platform,
-      status,
-    } = morningTrain;
-    const html = Template({
-      content: {
-        aimedDepartureTime,
-        destination,
-        expectedDepartureTime,
-        operator,
+    const data = await getDepartures();
+    const morningTrain = data.departures.all.find(train => train.train_uid === amTrainId);
+
+    if (typeof morningTrain === 'undefined') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(Error());
+    } else {
+      const {
+        aimed_departure_time: aimedDepartureTime,
+        destination_name: destination,
+        expected_departure_time: expectedDepartureTime,
+        operator_name: operator,
         platform,
         status,
-      },
-      title: status,
-    });
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(html);
+      } = morningTrain;
+      const html = Template({
+        content: {
+          aimedDepartureTime,
+          destination,
+          expectedDepartureTime,
+          operator,
+          platform,
+          status,
+        },
+        title: status,
+      });
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(html);
+    }
   }
 });
 
---
+++ error.js
@@ -0,0 +1,24 @@
+const Error = () => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Error</title>
+        <link rel="stylesheet" type="text/css" href="./style.css">
+      </head>
+
+      <body>
+        <main>
+          <h1>
+            Train not found
+          </h1>
+        </main>
+      </body>
+    </html>
+  `;
+};
+
+module.exports = Error;
